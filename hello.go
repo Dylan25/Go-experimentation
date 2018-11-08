967ccdc7b1b727b730cee227067059cda9b60e2d
Dylan <dtwelch@ucsc.edu>
2018-11-08 11:49:51 -0800

    added loop

--- hello.go
+++ hello.go
@@ -4,4 +4,17 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world")
+
+	foo()
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
 }
