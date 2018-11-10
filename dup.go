c2c4ed635d3817d3b900e7ca4414e6ad95e69baa
Dylan <dtwelch@ucsc.edu>
2018-11-10 11:32:09 -0800

    dup.go prints duplicated lines from stdin or file input

---
+++ dup.go
@@ -0,0 +1,38 @@
+// dup2 prints the count of any duplicated line from stdin or any given files followed
+// by the text of that line.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dp2: %v\n", err)
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for line, lcount := range counts {
+		if lcount > 1 {
+			fmt.Printf("%d\t%s\n", lcount, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
